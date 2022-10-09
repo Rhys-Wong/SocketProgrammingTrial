e480e8c5fbd93e5560effbd69858a824088d30a5
Rhys Wong <115345544+Rhys-Wong@users.noreply.github.com>
2022-10-10 00:44:19 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,25 +1,66 @@
-import java.io.*;
 import java.net.*;
-import java.util.*;
-
+import java.io.*;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String args[]) throws IOException{
-        Socket s = new Socket("localhost", 4999);
+    public static void main(String[] args) throws Exception {
+
+        Socket s = null;
+        InputStreamReader input = null;
+        OutputStreamWriter output = null;
+        BufferedWriter brw = null;
+        BufferedReader brr = null;
+
+        try{
+            s = new Socket("localhost",4999);
+            input = new InputStreamReader(s.getInputStream());
+            output = new OutputStreamWriter(s.getOutputStream());
+            brw = new BufferedWriter(output);
+            brr = new BufferedReader(input);
+
+            Scanner sc = new Scanner(System.in);
+
+            while(true){
+
+                String msgtosend = sc.nextLine();
+                brw.write(msgtosend);
+                brw.newLine();
+                brw.flush();
+                
+
+                System.out.println("Server : " + brr.readLine());
+
+                if(msgtosend.equalsIgnoreCase("Disconnect")){
+                    break;
+                }
+            } 
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        } 
+        finally {
+            try {
+                if(s != null){
+                    s.close();
+                }
+                if(input != null){
+                    input.close();
+                }
+                if(output != null){
+                    output.close();
+                }
+                if(brw != null){
+                    brw.close();
+                }
+                if(brr != null){
+                    brr.close();
+                }
+                    
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
         
-
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("Hello World! This is a test!");
-        pr.flush();
-
-        InputStreamReader input = new InputStreamReader(s.getInputStream());
-        BufferedReader br = new BufferedReader(input);
-
-        String str = br.readLine();
-
-        System.out.println("Server : " + str);
-
     }
-    
-    
 }
